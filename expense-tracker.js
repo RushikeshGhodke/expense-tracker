29111851586bdeccf9e69fd8a5b4c14e6cf7f2aa
RushikeshGhodke <rushikeshghodke7455@gmail.com>
2025-10-15 20:39:06 +0530

    Refactor expense-tracker commands to improve data handling and add error checking for missing data file

--- expense-tracker.js
+++ expense-tracker.js
@@ -1,27 +1,57 @@
 import { Command } from "commander";
 import fs from "fs";
 import path from 'path';
+import { fileURLToPath } from "url";
 
 const program = new Command();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dataFilePath = path.join(__dirname, 'data.json');
+
+// expense-tracker list
 program
-  .option('-n, --name <string>')
-  .option('-a, --age <number>')
-  .option('-l, --list')
+  .command('list')
+  .action(() => {
+    if (!fs.existsSync(dataFilePath)) {
+      console.error("Data file doesn't exists.");
+      return;
+    }
+
+    if (options.list) {
+      const jsonData = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'))
+      console.table(jsonData);
+    }
+  })
+
+program
+  .command('add')
+  .option('--description <string>')
+  .option('--amount <number>')
+  .action((options) => {
+
+    const jsonData = fs.readFileSync(dataFilePath) ? JSON.parse(fs.readFileSync(dataFilePath, 'utf8')) : [];
+
+    // Generate next ID
+    const nextId = jsonData.length > 0 ? jsonData[jsonData.length - 1].id + 1 : 1;
+
+    // Format date as YYYY-MM-DD
+    const formattedDate = new Date().toISOString().split("T")[0];
+
+    const newExpense = {
+      id: nextId,
+      description: options.description,
+      amount: parseInt(options.amount),
+      date: formattedDate
+    }
+
+    jsonData.push(newExpense);
+    fs.writeFileSync(dataFilePath, JSON.stringify(jsonData, null, 2))
+    console.table(jsonData);
+  })
 
 program.parse();
 
 const options = program.opts();
 
-const dataFilePath = path.join(import.meta.dirname, 'data.json');
 
-if (!fs.existsSync(dataFilePath)) {
-  console.error("Data file doesn't exists.");
-}
-
-// expense-tracker list
-
-if (options.list) {
-  const jsonData = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'))
-  console.table(jsonData);
-}
