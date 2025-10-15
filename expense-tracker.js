0899a2148ca9f2447359d474b1da8f3c88148319
RushikeshGhodke <rushikeshghodke7455@gmail.com>
2025-10-15 20:13:35 +0530

    Add list option to display expense data in table format

--- expense-tracker.js
+++ expense-tracker.js
@@ -7,19 +7,21 @@ const program = new Command();
 program
   .option('-n, --name <string>')
   .option('-a, --age <number>')
+  .option('-l, --list')
 
 program.parse();
 
 const options = program.opts();
-console.log(options.name);
 
 const dataFilePath = path.join(import.meta.dirname, 'data.json');
-
-console.log(dataFilePath);
 
 if (!fs.existsSync(dataFilePath)) {
   console.error("Data file doesn't exists.");
 }
 
-console.log(fs.readFileSync(dataFilePath, 'utf8'));
+// expense-tracker list
 
+if (options.list) {
+  const jsonData = JSON.parse(fs.readFileSync(dataFilePath, 'utf8'))
+  console.table(jsonData);
+}
